6ad8cff03f0dba57b1e40d31eaec52bbf3e89809
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2025-05-07 17:32:09 +0530

    Add APIs necessary for task listener

---
+++ native/src/main/java/io/ballerina/stdlib/task/listener/ListenerAction.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com)
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.ballerina.stdlib.task.listener;
+
+import io.ballerina.runtime.api.Environment;
+import io.ballerina.runtime.api.Runtime;
+import io.ballerina.runtime.api.concurrent.StrandMetadata;
+import io.ballerina.runtime.api.creators.ValueCreator;
+import io.ballerina.runtime.api.utils.StringUtils;
+import io.ballerina.runtime.api.utils.TypeUtils;
+import io.ballerina.runtime.api.values.BDecimal;
+import io.ballerina.runtime.api.values.BMap;
+import io.ballerina.runtime.api.values.BObject;
+import io.ballerina.runtime.api.values.BString;
+import io.ballerina.stdlib.task.objects.TaskManager;
+import io.ballerina.stdlib.task.utils.ModuleUtils;
+import io.ballerina.stdlib.task.utils.Utils;
+
+public class ListenerAction {
+    public static final String NATIVE_LISTENER_KEY = "TASK_NATIVE_LISTENER";
+    public static final BString TRIGGER_TIME = StringUtils.fromString("triggerTime");
+    public static final BString INTERVAL = StringUtils.fromString("interval");
+    public static final BString MAX_COUNT = StringUtils.fromString("maxCount");
+    public static final BString START_TIME = StringUtils.fromString("startTime");
+    public static final BString END_TIME = StringUtils.fromString("endTime");
+    public static final BString TASK_POLICY = StringUtils.fromString("taskPolicy");
+    public static final String ONE_TIME_CONFIGURATION = "OneTimeConfiguration";
+    public static final String LISTENER_NOT_INITIALIZED_ERROR = "Listener not initialized";
+
+    public static void initListener(BObject listener, BMap<BString, Object> listenerConfig) {
+        TaskListener taskListener = new TaskListener();
+        BMap<?, ?> schedule = listenerConfig.getMapValue(StringUtils.fromString("schedule"));
+        taskListener.setType(TypeUtils.getType(schedule).getName());
+        if (TypeUtils.getType(schedule).getName().contains(ONE_TIME_CONFIGURATION)) {
+            Runtime runtime = TaskManager.getInstance().getRuntime();
+            StrandMetadata metadata = new StrandMetadata(true, null);
+            BObject timeConverter =
+                    ValueCreator.createObjectValue(ModuleUtils.getModule(), "TimeConverter");
+            long triggerTime = (long) runtime.callMethod(timeConverter, "getTimeInMillies", metadata);
+            taskListener.setConfig(TRIGGER_TIME, triggerTime);
+        } else {
+            taskListener.setConfigs(schedule);
+        }
+        listener.addNativeData(NATIVE_LISTENER_KEY, taskListener);
+    }
+
+    public static Object startListener(Environment environment, BObject listenerObj) {
+        TaskListener listener = (TaskListener) listenerObj.getNativeData(NATIVE_LISTENER_KEY);
+        if (listener != null) {
+            if (listener.getType().equals(ONE_TIME_CONFIGURATION)) {
+                long triggerTime = (long) listener.getConfig().get(TRIGGER_TIME);
+                listener.start(environment, listenerObj, triggerTime);
+            } else {
+                listener.start(environment, listenerObj,
+                        (BDecimal) listener.getConfig().get(INTERVAL),
+                        (Long) listener.getConfig().get(MAX_COUNT),
+                        listener.getConfig().get(START_TIME),
+                        listener.getConfig().get(END_TIME),
+                        (BMap) listener.getConfig().get(TASK_POLICY));
+            }
+        } else {
+            return Utils.createTaskError(LISTENER_NOT_INITIALIZED_ERROR);
+        }
+        return null;
+    }
+
+    public static Object attachService(BObject listenerObj, BObject service, BString serviceName) {
+        TaskListener listener = (TaskListener) listenerObj.getNativeData(NATIVE_LISTENER_KEY);
+        listener.registerService(serviceName.getValue(), service);
+        return null;
+    }
+
+    public static Object detachService(BObject listenerObj, BObject service, BString serviceName) {
+        TaskListener listener = (TaskListener) listenerObj.getNativeData(NATIVE_LISTENER_KEY);
+        listener.unregisterService(serviceName.getValue());
+        return null;
+    }
+}
+
---
+++ native/src/main/java/io/ballerina/stdlib/task/listener/TaskListener.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com)
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.ballerina.stdlib.task.listener;
+
+import io.ballerina.runtime.api.Environment;
+import io.ballerina.runtime.api.creators.ValueCreator;
+import io.ballerina.runtime.api.values.BDecimal;
+import io.ballerina.runtime.api.values.BMap;
+import io.ballerina.runtime.api.values.BObject;
+import io.ballerina.runtime.api.values.BString;
+import io.ballerina.stdlib.task.exceptions.SchedulingException;
+import io.ballerina.stdlib.task.objects.TaskManager;
+import io.ballerina.stdlib.task.utils.TaskConstants;
+import io.ballerina.stdlib.task.utils.Utils;
+import org.quartz.JobDataMap;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+
+import java.math.BigDecimal;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map;
+
+public class TaskListener {
+    private static final int bound = 1000000;
+    private static final String value = "1000";
+    private String type;
+    private final Map<String, BObject> serviceRegistry = new ConcurrentHashMap<>();
+    private BMap<BString, Object> configs = ValueCreator.createMapValue();
+    private Boolean isTaskCoordinator = false;
+
+    public TaskListener() {
+
+    }
+
+    public void start(Environment env, BObject job, long time) {
+        Utils.disableQuartzLogs();
+        try {
+            getScheduler(env);
+            for (String serviceName : serviceRegistry.keySet()) {
+                Integer jobId = java.security.SecureRandom.getInstanceStrong().nextInt(bound);
+                JobDataMap jobDataMap = getJobDataMap(job, TaskConstants.LOG_AND_CONTINUE, String.valueOf(jobId));
+                BObject service = serviceRegistry.get(serviceName);
+                TaskManager.getInstance().scheduleOneTimeListenerJob(jobDataMap, time, jobId, service);
+            }
+        } catch (SchedulerException | SchedulingException | IllegalArgumentException | NoSuchAlgorithmException e) {
+        }
+    }
+
+    public void start(Environment env, BObject job, BDecimal interval, long maxCount,
+                      Object startTime, Object endTime, BMap<BString, Object> policy) {
+        try {
+            getScheduler(env);
+            for (String serviceName : serviceRegistry.keySet()) {
+                int jobId = java.security.SecureRandom.getInstanceStrong().nextInt(bound);
+                JobDataMap jobDataMap = getJobDataMap(job, ((BString) policy.get(TaskConstants.ERR_POLICY)).getValue(),
+                        String.valueOf(jobId));
+                BObject service = serviceRegistry.get(serviceName);
+                TaskManager.getInstance().scheduleListenerIntervalJob(jobDataMap,
+                        (interval.decimalValue().multiply(new BigDecimal(value))).longValue(), maxCount, startTime,
+                        endTime, ((BString) policy.get(TaskConstants.WAITING_POLICY)).getValue(), jobId, service);
+            }
+
+        } catch (Exception e) {
+        }
+    }
+
+    public void setConfig(BString key, Object value) {
+        configs.put(key, value);
+    }
+
+    public void setConfigs(BMap<?, ?> values) {
+        configs.merge(values, false);
+    }
+
+    public BMap<BString, Object> getConfig() {
+        return configs;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Boolean isTaskCoordinator() {
+        return isTaskCoordinator;
+    }
+
+    public void setTaskCoordinator(Boolean isTaskCoordinator) {
+        this.isTaskCoordinator = isTaskCoordinator;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void registerService(String serviceName, BObject service) {
+        serviceRegistry.put(serviceName, service);
+    }
+
+    public void unregisterService(String serviceName) {
+        serviceRegistry.remove(serviceName);
+    }
+
+    public BObject getService(String serviceName) {
+        return serviceRegistry.get(serviceName);
+    }
+
+    private static Scheduler getScheduler(Environment env) throws SchedulingException, SchedulerException {
+        Utils.disableQuartzLogs();
+        return TaskManager.getInstance().getScheduler(Utils.createSchedulerProperties(
+                TaskConstants.QUARTZ_THREAD_COUNT_VALUE, TaskConstants.QUARTZ_THRESHOLD_VALUE), env);
+    }
+
+    private static JobDataMap getJobDataMap(BObject job, String errorPolicy, String jobId) {
+        Utils.disableQuartzLogs();
+        JobDataMap jobDataMap = new JobDataMap();
+        jobDataMap.put(TaskConstants.JOB, job);
+        jobDataMap.put(TaskConstants.ERROR_POLICY, errorPolicy);
+        jobDataMap.put(TaskConstants.JOB_ID, jobId);
+        return jobDataMap;
+    }
+}
