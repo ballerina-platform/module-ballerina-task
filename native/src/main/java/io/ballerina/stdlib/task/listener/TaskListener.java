7afb96eb95d934125e71e7b681e345a86a902775
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2025-05-08 14:34:57 +0530

    Fix scheduling in coordination

--- native/src/main/java/io/ballerina/stdlib/task/listener/TaskListener.java
+++ native/src/main/java/io/ballerina/stdlib/task/listener/TaskListener.java
@@ -39,17 +39,19 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class TaskListener {
-    private final TaskManager taskManager;
-    private String type;
-    private static final int bound = 1000000;
-    private static final String value = "1000";
     public static final BString DATABASE_CONFIG = StringUtils.fromString("databaseConfig");
     public static final BString TASK_ID = StringUtils.fromString("taskId");
     public static final BString GROUP_ID = StringUtils.fromString("groupId");
     public static final BString LIVENESS_CHECK_INTERVAL = StringUtils.fromString("livenessCheckInterval");
     public static final BString HEARTBEAT_FREQUENCY = StringUtils.fromString("heartbeatFrequency");
+
+    private static final String VALUE = "1000";
+
+    private final TaskManager taskManager;
     private final Map<String, BObject> serviceRegistry = new ConcurrentHashMap<>();
     private final BMap<BString, Object> configs = ValueCreator.createMapValue();
+
+    private String type;
 
     public TaskListener(TaskManager taskManager, String type) {
         this.taskManager = taskManager;
@@ -78,7 +80,7 @@ public class TaskListener {
                     serviceName);
             BObject service = serviceRegistry.get(serviceName);
             this.taskManager.scheduleListenerIntervalJob(jobDataMap,
-                    (interval.decimalValue().multiply(new BigDecimal(value))).longValue(), maxCount, startTime,
+                    (interval.decimalValue().multiply(new BigDecimal(VALUE))).longValue(), maxCount, startTime,
                     endTime, ((BString) policy.get(TaskConstants.WAITING_POLICY)).getValue(), serviceName, service);
         }
     }
@@ -88,9 +90,8 @@ public class TaskListener {
                       BMap warmBackupConfig) throws Exception {
         getScheduler(env);
         for (String serviceName : serviceRegistry.keySet()) {
-            int jobId = java.security.SecureRandom.getInstanceStrong().nextInt(bound);
             JobDataMap jobDataMap = getJobDataMap(job, ((BString) policy.get(TaskConstants.ERR_POLICY)).getValue(),
-                    String.valueOf(jobId));
+                    serviceName);
             BObject service = serviceRegistry.get(serviceName);
             BMap<Object, Object> databaseConfig = warmBackupConfig.getMapValue(DATABASE_CONFIG);
             BString id = warmBackupConfig.getStringValue(TASK_ID);
@@ -100,9 +101,9 @@ public class TaskListener {
             BMap response = (BMap) TokenAcquisition.acquireToken(databaseConfig, id, groupId, false,
                     livenessInterval, heartbeatFrequency);
             this.taskManager.scheduleListenerIntervalJobWithTokenCheck(jobDataMap,
-                    (interval.decimalValue().multiply(new BigDecimal(value))).longValue(), maxCount, startTime,
+                    (interval.decimalValue().multiply(new BigDecimal(VALUE))).longValue(), maxCount, startTime,
                     endTime, ((BString) policy.get(TaskConstants.WAITING_POLICY)).getValue(),
-                    jobId, response, service);
+                    serviceName, response, service);
         }
     }
 
